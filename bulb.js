e6ec98bb4d2ecebbc1cafb08d6f81e15c0fe1970
Adeel-Muhammad <126050873+Adeel-Muhammad@users.noreply.github.com>
2023-09-03 02:51:23 +0500

    update class and id of the img tag

---
+++ bulb.js
@@ -0,0 +1,32 @@
+const btn1= document.getElementById("btn1");
+const btn2= document.getElementById("btn2");
+const btn3= document.getElementById("btn3");
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3= document.getElementById("img3");
+
+
+btn1.addEventListener("click",chnageColor1);
+
+
+function chnageColor1(){
+
+    btn1.textContent="turn off"
+    
+
+}
+
+btn2.addEventListener("click",chnageColor2);
+
+function chnageColor2(){
+
+    btn2.textContent="turn off"
+
+}
+btn3.addEventListener("click",chnageColor3);
+
+function chnageColor3(){
+
+    btn3.textContent="turn off"
+
+}
